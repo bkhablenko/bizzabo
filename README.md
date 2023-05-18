cb8d84c87cb043f22809c7ede758441585c4147a
Bohdan Khablenko <bkhablenko@gmail.com>
2023-05-18 16:45:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+bizzabo
+=======
+
+## How to run
+
+```bash
+./gradlew clean build -x test && docker compose up --build
+```
+
+API documentation will be available at http://localhost:8090.
---
+++ src/main/kotlin/com/github/bkhablenko/bizzabo/config/WebSecurityConfig.kt
@@ -0,0 +1,42 @@
+package com.github.bkhablenko.bizzabo.config
+
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableMethodSecurity
+class WebSecurityConfig {
+
+    companion object {
+
+        /**
+         * @see [UserDetailsServiceAutoConfiguration.NOOP_PASSWORD_PREFIX]
+         */
+        private const val NOOP_PASSWORD_PREFIX = "{noop}"
+    }
+
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        httpSecurity {
+            csrf { disable() }
+            authorizeRequests {
+                authorize(anyRequest, permitAll)
+            }
+            httpBasic {}
+        }
+        return httpSecurity.build()
+    }
+
+    /**
+     * Allow any username to authenticate as long as the password is empty.
+     */
+    @Bean
+    fun userDetailsService() = UserDetailsService { username -> User(username, NOOP_PASSWORD_PREFIX, emptyList()) }
+}
