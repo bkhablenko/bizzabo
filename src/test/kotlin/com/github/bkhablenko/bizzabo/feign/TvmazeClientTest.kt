dd50aad5e188b2f0f6e8477c51c7d07c9d8fd54b
Bohdan Khablenko <bkhablenko@gmail.com>
2023-05-19 10:46:02 +0300

    Update TvmazeClientTest

--- src/test/kotlin/com/github/bkhablenko/bizzabo/feign/TvmazeClientTest.kt
+++ src/test/kotlin/com/github/bkhablenko/bizzabo/feign/TvmazeClientTest.kt
@@ -1,14 +1,18 @@
 package com.github.bkhablenko.bizzabo.feign
 
+import com.github.bkhablenko.bizzabo.feign.model.TvmazeCastMember
 import com.github.bkhablenko.bizzabo.feign.model.TvmazeImage
 import com.github.bkhablenko.bizzabo.feign.model.TvmazeShow
 import com.github.bkhablenko.bizzabo.feign.model.TvmazeShowEpisode
+import feign.FeignException
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasItem
 import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import java.net.URL
 import java.time.LocalDate
@@ -39,11 +43,49 @@ class TvmazeClientTest : AbstractFeignClientTest() {
             )
             assertThat(tvmazeClient.getShowById(GAME_OF_THRONES_SHOW_ID), equalTo(expectedShow))
         }
+
+        @Test
+        fun `should throw exception if not found`() {
+            val nonExistentShowId = 0
+            assertThrows<FeignException.NotFound> {
+                tvmazeClient.getShowById(nonExistentShowId)
+            }
+        }
     }
 
-    @DisplayName("getShowEpisodesById")
+    @DisplayName("getCastByShowId")
     @Nested
-    inner class GetShowEpisodesByIdTest {
+    inner class GetCastByShowIdTest {
+
+        @Test
+        fun `should return expected cast`() {
+            val result = tvmazeClient.getCastByShowId(GAME_OF_THRONES_SHOW_ID)
+            assertThat(result, hasSize(42))
+
+            val expectedCastMember = TvmazeCastMember(
+                person = TvmazeCastMember.Person(
+                    id = 14075,
+                    name = "Kit Harington",
+                    image = TvmazeImage(
+                        original = URL("https://static.tvmaze.com/uploads/images/original_untouched/1/3229.jpg"),
+                    ),
+                ),
+            )
+            assertThat(result, hasItem(expectedCastMember))
+        }
+
+        @Test
+        fun `should throw exception if not found`() {
+            val nonExistentShowId = 0
+            assertThrows<FeignException.NotFound> {
+                tvmazeClient.getCastByShowId(nonExistentShowId)
+            }
+        }
+    }
+
+    @DisplayName("getEpisodesByShowId")
+    @Nested
+    inner class GetEpisodesByShowIdTest {
 
         @Test
         fun `should return expected TV show episodes`() {
@@ -58,7 +100,15 @@ class TvmazeClientTest : AbstractFeignClientTest() {
                 airdate = LocalDate.of(2011, Month.APRIL, 17)
 
             )
-            assertThat(result.first(), equalTo(expectedFirstEpisode))
+            assertThat(result, hasItem(expectedFirstEpisode))
+        }
+
+        @Test
+        fun `should throw exception if not found`() {
+            val nonExistentShowId = 0
+            assertThrows<FeignException.NotFound> {
+                tvmazeClient.getEpisodesByShowId(nonExistentShowId)
+            }
         }
     }
 }
